d75501689822811593143ffc8abc559074cbe303
Sammy Robens-Paradise <srobensparadise@gmail.com>
2021-02-28 22:44:38 -0500

    template

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import { getGithubContributions } from 'github-contributions-counter'
+import JSONPretty from 'react-json-pretty'
 
-function App() {
+import './App.css'
+
+const App = () => {
+  const [username, setUsername] = useState('')
+  const [results, updateResults] = useState(null)
+  const handleSubmit = async (event) => {
+    event.preventDefault()
+    const res = await getGithubContributions({ username: username })
+    console.log(res)
+    updateResults(res)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>
+        <code>npm i github-contributions-counter</code>
+      </h1>
+      <p>Enter a username and click "Submit" to try it out!</p>
+      <div>
+        <form onSubmit={(event) => handleSubmit(event)}>
+          <input
+            type="text"
+            id="username-input"
+            placeholder="SammyRobensParadise"
+            style={{ width: '60%' }}
+            autoFocus
+            onChange={(event) => {
+              setUsername(event.target.value)
+            }}
+          />
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
+      <div style={{ marginTop: '40px' }}>
+        <JSONPretty
+          id="json-pretty"
+          data={JSON.stringify(results)}
+        ></JSONPretty>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
